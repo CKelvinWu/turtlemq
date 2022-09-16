f367d5ed80e581048ef686e8735e5ccae2199ca9
kai5363 <57265307+kai5363@users.noreply.github.com>
2022-09-16 22:08:29 +0800

    feat: render queue with proper start and end time

--- turtleWatcher/models/queue.js
+++ turtleWatcher/models/queue.js
@@ -3,15 +3,24 @@ const { redis } = require('../utils/redis');
 
 const { QUEUE_LIST, HISTORY_KEY } = process.env;
 const HISTORY_TIME = 60 * 60 * 1000;
+const HISTORY_INTERVAL = 5000;
 
 const getQueue = async () => {
   const queueList = await redis.smembers(QUEUE_LIST);
   const queueInfo = {};
   const time = Date.now();
   for (const queue of queueList) {
-    const results = await redis.lrange(`${HISTORY_KEY}${queue}`, 0, -1);
-    // if results.lenth = 0
+    const results = await redis.lrange(
+      `${HISTORY_KEY}${queue}`,
+      -(HISTORY_TIME / HISTORY_INTERVAL) - 1,
+      -1,
+    );
+
     const data = [];
+    if ((HISTORY_TIME / HISTORY_INTERVAL) + 1 > results.length) {
+      data.push({ time: time - (+HISTORY_TIME), queueSize: 0 });
+    }
+
     for (let i = 0; i < results.length; i++) {
       const history = JSON.parse(results[i]);
       // if there is any history in past an hour
@@ -19,18 +28,7 @@ const getQueue = async () => {
         data.push(history);
       }
     }
-    // if there is non history in past an hour
-    if (!data.at(-1)?.queueSize) {
-      const result = await redis.lindex(`${HISTORY_KEY}${queue}`, -1);
-      const history = JSON.parse(result);
-      data.push(
-        { time: time - (+HISTORY_TIME), queueSize: history.queueSize },
-        // { time: time - 5000, queueSize: history.queueSize },
-        // { time, queueSize: history.queueSize },
-      );
-    } else {
-      data.push({ time: Date.now(), queueSize: data.at(-1)?.queueSize });
-    }
+    data.push({ time, queueSize: data.at(-1)?.queueSize });
     queueInfo[queue] = data;
   }
   return queueInfo;
--- turtleWatcher/public/js/monitor.js
+++ turtleWatcher/public/js/monitor.js
@@ -1,3 +1,4 @@
+/* eslint-disable prefer-arrow-callback */
 $(() => {
   /*
   * Flot Interactive Chart
@@ -7,79 +8,97 @@ $(() => {
   // be fetched from a server
   const updateInterval = 5000; // Fetch data ever x milliseconds
   const realtime = 'on'; // If == to on then fetch data every x seconds. else stop fetching
-  const historyTime = 60 * 60 * 1000;
 
-  function getData(result) {
-    const currentTime = Date.now();
-    let index = 1;
-    const queue = result.competition;
-
+  function getData(queue) {
+    // const currentTime = Date.now();
+    // const index = 1;
     const res = [];
-    for (let time = currentTime - historyTime; time < currentTime; time += 5000) {
-      if (queue.length === 1) {
-        res.push([time, queue[0]?.queueSize]);
-      } else if (queue[index]?.time > time) {
-        res.push([time, queue[index - 1]?.queueSize]);
-      } else {
-        index++;
-        res.push([time, queue[index - 1]?.queueSize]);
-      }
+
+    for (let i = 0; i < queue.length; i++) {
+      res.push([queue[i]?.time, queue[i]?.queueSize]);
     }
-    res.push([currentTime, queue[index - 1]?.queueSize]);
     return res;
+  }
+
+  function createPlot(name) {
+    let chartBody = $('<div></div>').addClass('card-body').attr('id', `queue-${name}`);
+    const queueTitleH3 = $('<h3></h3>').addClass('card-title').text(name);
+    const interactiveDiv = $('<div></div>').addClass('interactive').attr('id', `interactive-${name}`).attr('data-queue', name)
+      .css({ height: '300px', padding: '0px', position: 'relative' });
+    chartBody = chartBody.append(queueTitleH3);
+    chartBody = chartBody.append(interactiveDiv);
+    $('#chart-container').append(chartBody);
+  }
+  function deletePlot(name) {
+    $(`#queue-${name}`).remove();
   }
 
   function update() {
     $.ajax('http://localhost:3000/api/1.0/queue')
+
       .done((result) => {
-        getData(result);
-        const currentTime = Date.now();
-        const start = currentTime - historyTime;
-        const end = currentTime;
-        const step = 6 * 60 * 1000;
-        const arrayLength = Math.floor(((end - start) / step)) + 1;
-        const range = [...Array(arrayLength).keys()].map((x) => (x * step) + start);
-        const interactivePlot = $.plot(
-          '#interactive',
-          [
+        const queueName = Object.keys(result);
+        $('.interactive').each(function () {
+          const queue = $(this).data('queue');
+          const isExistQueue = queueName.includes(queue);
+          if (!isExistQueue) {
+            deletePlot(queue);
+          }
+        });
+
+        queueName.forEach((name) => {
+          const isPlot = $(`#interactive-${name}`).length;
+          if (!isPlot) {
+            createPlot(name);
+          }
+          const res = getData(result[name]);
+          const start = res[0][0];
+          const end = res.at(-1)[0];
+          const step = Math.floor((end - start) / 15);
+          const arrayLength = Math.floor(((end - start) / step)) + 1;
+          const range = [...Array(arrayLength).keys()].map((x) => (x * step) + start);
+          const interactivePlot = $.plot(
+            `#interactive-${name}`,
+            [
+              {
+                label: 'Foo', data: getData(result[name]),
+              },
+            ],
             {
-              label: 'Foo', data: getData(result),
-            },
-          ],
-          {
-            grid: {
-              borderColor: '#f3f3f3',
-              borderWidth: 1,
-              tickColor: '#f3f3f3',
-            },
-            series: {
-              color: '#3c8dbc',
-              lines: {
-                lineWidth: 2,
+              grid: {
+                borderColor: '#f3f3f3',
+                borderWidth: 1,
+                tickColor: '#f3f3f3',
+              },
+              series: {
+                color: '#3c8dbc',
+                lines: {
+                  lineWidth: 2,
+                  show: true,
+                  fill: true,
+                },
+              },
+              yaxis: {
+                ticks: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 100],
+                min: 0,
+                max: 100,
                 show: true,
-                fill: true,
+              },
+              xaxis: {
+                ticks: range,
+                mode: 'time',
+                timeBase: 'seconds',
+                timeformat: '%H:%M:%S',
+                min: 0,
+                max: 100,
+                show: true,
               },
             },
-            yaxis: {
-              ticks: [0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100, 100],
-              min: 0,
-              max: 100,
-              show: true,
-            },
-            xaxis: {
-              ticks: range,
-              mode: 'time',
-              timeBase: 'seconds',
-              timeformat: '%H:%M:%S',
-              min: 0,
-              max: 100,
-              show: true,
-            },
-          },
-        );
-        // interactivePlot.setData([getData()]);
-        // Since the axes don't change, we don't need to call plot.setupGrid()
-        interactivePlot.draw();
+          );
+          // interactivePlot.setData([getData()]);
+          // Since the axes don't change, we don't need to call plot.setupGrid()
+          interactivePlot.draw();
+        });
         if (realtime === 'on') {
           setTimeout(update, updateInterval);
         }
